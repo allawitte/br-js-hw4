ad231d2577fdff5260fd77cc9d0483ebdae75dc8
= <alla_88@inbox.lv>
2017-07-17 17:59:06 -0300

    corrections are done

--- test1/js/script.js
+++ test1/js/script.js
@@ -1,11 +1,10 @@
 const contacts = JSON.parse(loadContacts());
 const listPreview = document.querySelectorAll('ul.contacts-list')[0];
-var listDetails = document.querySelectorAll('.details-view ul')[0];
 
 function createPrevList(contacts) {
 
     return contacts.reduce((sum, item) => {
-       return sum = sum + '<li data-email="'+ item.email + '" data-phone="' + item.phone + '"><strong>' + item.name + '</strong></li>';
+       return  sum + '<li data-email="'+ item.email + '" data-phone="' + item.phone + '"><strong>' + item.name + '</strong></li>';
     },'');
 }
 
